1e39f92894cbf2c6c53bfa250b6525275b6b7c13
zyx <zyx134@163.com>
2019-07-19 11:38:55 -0400

    level order

--- binary_tree.py
+++ binary_tree.py
@@ -117,9 +117,29 @@ class Solution:
                 stack.append(node.right)
         return result[::-1]
         
+
+# Level Order Traversal: Using a queue to record the visited node
+
+class Solution:
+	
+	def leverOrderTraversal(self, root):
+		if not root:
+			return []
+		queue = [root]
+		result = []
 		
+		while queue:
+			level = []
+			for i in range(len(queue))
+				node = queue.pop(0)
+				level.append(node.val)
+				if node.left:
+					queue.append(node.left)
+				if node.right:
+					queue.append(node.right)
+			result.insert(0, level)
 		
-		
+		return result		
 		
 		
 		
