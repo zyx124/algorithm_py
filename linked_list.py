c2ef7df8255c1a6309ce3b99560c1e953ed4ccdd
zyx <zyx134@163.com>
2019-07-27 11:43:14 -0400

    linked list and dfs

--- linked_list.py
+++ linked_list.py
@@ -9,11 +9,13 @@ Linked List basic methods:
 - delete a node
 - reverse from mth to nth
 - find the middle 
-- merge two linked lists
+- merge two linked lists  --> merge K sorted linked list 
+- copy a list
+
 
 Some tricks: 
 - use a dummy node when the structure of the linked list changes
-- use pointers to record the positions of some nodes 
+- use fast and slow pointers to record the positions of some nodes --> detect cycle
 '''
 #
 
@@ -90,4 +92,75 @@ def mergeTwoLists(l1, l2):
 		tail.next = l2
 	
 	return dummy.next
+	
+	
+# detect cycle: fast and slow pointers, if there is a cycle, fast pointer will catch up with the slow one.
+class Solution:
+	
+	def hasCycle(self, head):
+		if not head:
+			return False
+		fast = head
+		slow = head
+		while fast and fast.next:
+			fast = fast.next.next
+			slow = slow.next
+			if fast == slow:
+				return True
+		return False
+		
+	# method to find where cycle begins: suppose the length of the cycle is l, the distance from the start of the cycle to the head is a, the place where the fast catches up with the slow has distance b with respect to the start of the cycle, the other part of the cycle will be c = (l - b). Since the fast pointer is twice the speed of the slow one, a + l + b = 2 * (a + b) ===> a = c. Therefore, after the fast pointer meet the slow one, if we move a pointer from the head with the same speed with the fast pointer, the place where they meet will be the starting point of the cycle.
+	
+	def cycle_start(self, head):
+		if head is None or head.next is None:
+			return None
+		slow = head
+		fast = head
+		while fast and fast.next:
+			slow = slow.next
+			fast = fast.next.next
+			if slow == fast:
+				break
+				
+		if slow == fast:
+			slow = head
+			while slow != fast:
+				slow = slow.next
+				fast = fast.next
+			return slow
+			
+		return None
 
+## copy a list with a random pointer --> hash map
+class RandomListNode:
+    def __init__(self, x):
+        self.label = x
+        self.next = None
+        self.random = None
+        
+class Solution:
+	
+	def copy_ramdom_list(self, head):
+		my_map = {}
+		nHead = RandomListNode(head.label)
+		my_map[head] = nHead
+		p = head
+		q = nHead
+		while p:
+			q.random = p.random   # the copied node's random pointer still points to the original list's node 
+			if p.next:
+				q.next = RandomListNode(p.next.label)
+				my_map[p.next] = q.next
+			else:
+				q.next = None
+			p = p.next
+			q = q.next
+			
+		p = nHead
+		
+		while p:
+			if p.random:
+				p.random = my_map[p.random]
+			p = p.next
+		
+		return nHead
--- permutation_combination_search.py
+++ permutation_combination_search.py
@@ -1,10 +1,11 @@
 '''
-Combination and permutation search often use DFS to go through all of possible results bit
+Partition, combination and permutation often use DFS to search all of possible results bit
 by bit. Backtrack is a commonly used technique.
 '''
 
 ## permutation of a list of numbers: [1,3,4,54,5....]
 class Solution:
+
 	def permute(self, nums):
         # write your code here
         self.result = []
@@ -12,7 +13,6 @@ class Solution:
         return self.result
     
     def dfs(self, nums, temp):
-        
         if len(temp) == len(nums):
             self.result.append(temp.copy())
             
@@ -38,3 +38,30 @@ class Solution:
 			subset.append(nums[i])
 			self.dfs(nums, k, subset, i+1)
 			subset.pop()
+			
+## find subsets -> find the combinations of k numbers out of a list
+## where k is from 0 to len(list). Example: [0] -> [[], [0]]
+class Solution:
+	
+	def subset(self, nums):
+		if nums is None:
+			return 
+		nums.sort()
+		result = []
+		subset = []
+		index = 0
+		self.dfs(nums, subset, index, result)
+		return result
+		
+	def dfs(self, nums, subset, index, result):
+		result.append(subset.copy())
+		for i in range(index, len(nums)):
+			subset.append(nums[i])
+			self.dfs(nums, subset, i+1, result)
+			subset.pop()
+		
+			
+			
+			
+			
+			
