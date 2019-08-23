49d281cec2231d4b0da19a36c2fa0435eefa0a74
zyx <zyx134@163.com>
2019-08-23 17:20:29 -0400

    add an example on bounds

--- binary_search.py
+++ binary_search.py
@@ -1,5 +1,12 @@
 #! /usr/lib/python
 
+# The general template for binary search is:
+# - find mid
+# - compare mid and target value to shrink the bound 
+# - check if the result after the loop is qualified answer.
+
+## Tips: when there is a certain range for the answer, the conditions to find left and right bound could be different.
+
 ## binary search a number 
 def binary_search(nums, target):
 	start, end = 0, len(nums) - 1
@@ -69,3 +76,45 @@ def search_matrix(matrix, target):
 		return False
 		
 	return True
+	
+# search for a range: Given a sorted array of n integers, find the starting and ending position of a given target value.
+# Keep an eye on the conditions when getting left and right bound. 
+class Solution:
+    """
+    @param A: an integer sorted array
+    @param target: an integer to be inserted
+    @return: a list of length 2, [index1, index2]
+    """
+    def searchRange(self, A, target):
+        # write your code here
+        if not A:
+            return [-1, -1]
+        start, end = 0, len(A) - 1
+        while start + 1 < end:
+            mid = start + (end - start) // 2 
+            if A[mid] < target:
+                start = mid
+            else:
+                end = mid
+        if A[start] == target:
+            left_bound = start
+        elif A[end] == target:
+            left_bound = end 
+        else:
+            return [-1, -1]
+            
+        start, end = 0, len(A) - 1
+        while start + 1 < end:
+            mid = start + (end - start) // 2 
+            if A[mid] <= target:
+                start = mid
+            else:
+                end = mid
+        if A[end] == target:
+            right_bound = end
+        elif A[start] == target:
+            right_bound = start
+        else:
+            return [-1, -1]
+            
+        return [left_bound, right_bound]
