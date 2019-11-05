5554c5d150a15245a943873cfb0ef3844d03a07e
zyx124 <zhaoyuxin124@gmail.com>
2019-11-04 22:30:33 -0500

    add two_pointer.py

---
+++ two_pointer.py
@@ -0,0 +1,8 @@
+"""
+Two-Pointer method is one of the most commonly used algorithms. The two pointers can be in the same direction or in opposite directions. This method is usually used to solve linear data structures, such as arrays and linked lists.
+
+classic problems:
+2Sum, 3Sum, 4Sum, partition
+
+
+"""
